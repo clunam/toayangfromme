14037b642aedb1994bb85f6885f7c26d4e9a81d6
clunam <clarissalunaaa@gmail.com>
2025-02-24 21:13:24 +0700

    ganti nama

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'game.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/messages.json', (req, res) => {
